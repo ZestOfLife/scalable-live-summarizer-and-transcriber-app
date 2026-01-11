efd19cdac9c652704510ebdf21d785ca0e5dbb63
ZestOfLife <jamescook371@gmail.com>
2026-01-11 19:56:52 +0700

    Restructure project structure

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"io"
 	"log"
 	"net"
@@ -10,53 +9,8 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	pb "github.com/ZestOfLife/scalable-live-summarizer-and-transcriber-app/api"
+	"github.com/ZestOfLife/scalable-live-summarizer-and-transcriber-app/internal/api/grpc"	
 )
-
-type Server struct {
-	pb.UnimplementedInferenceServiceServer
-	Client pb.InferenceServiceClient
-}
-
-func (s *Server) ProcessMedia(stream pb.InferenceService_ProcessMediaServer) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-
-	clientStream, err := s.Client.ProcessMedia(ctx)
-    	if err != nil {
-        	return err
-    	}
-
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			if err == io.EOF {
-				res, err := clientStream.CloseAndRecv()
-            			if err != nil {
-                			return err
-            			}
-				return stream.SendAndClose(res)
-			} else {
-				return err
-			}
-		}
-	
-
-		switch payload := req.Payload.(type) {
-		case *pb.MediaStreamRequest_Video:
-			payload.Video.Data = CompressData(payload.Video.Data)
-		case *pb.MediaStreamRequest_Audio:
-			payload.Audio.Data = CompressData(payload.Audio.Data)
-		case *pb.MediaStreamRequest_Seek:
-			// Do nothing cause this will be handled by the model server
-		}	
-
-		errClient := clientStream.Send(req)
-		if errClient != nil {
-			return errClient
-		}
-	}
-}
 
 func main() {
 	conn, err := grpc.Dial(os.Getenv("COMMAND_SERVER_ADDR") + ":" + os.Getenv("COMMAND_SERVER_PORT"), grpc.WithTransportCredentials(insecure.NewCredentials())) 
---
+++ internal/transport/grpc/handler.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"context"
+	"io"
+	"time"
+
+	pb "github.com/ZestOfLife/scalable-live-summarizer-and-transcriber-app/pkg/gen/proto/v1"
+)
+
+type Server struct {
+	pb.UnimplementedInferenceServiceServer
+	Client pb.InferenceServiceClient
+}
+
+func (s *Server) ProcessMedia(stream pb.InferenceService_ProcessMediaServer) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	clientStream, err := s.Client.ProcessMedia(ctx)
+    	if err != nil {
+        	return err
+    	}
+
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				res, err := clientStream.CloseAndRecv()
+            			if err != nil {
+                			return err
+            			}
+				return stream.SendAndClose(res)
+			} else {
+				return err
+			}
+		}
+	
+
+		switch payload := req.Payload.(type) {
+		case *pb.MediaStreamRequest_Video:
+			payload.Video.Data = CompressData(payload.Video.Data)
+		case *pb.MediaStreamRequest_Audio:
+			payload.Audio.Data = CompressData(payload.Audio.Data)
+		case *pb.MediaStreamRequest_Seek:
+			// Do nothing cause this will be handled by the model server
+		}	
+
+		errClient := clientStream.Send(req)
+		if errClient != nil {
+			return errClient
+		}
+	}
+}
